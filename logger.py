289e1d18dcc7959bf6169d03e6525b87745ea091
Nitheesh Kannan <nitheeshkannanshanmugavel@gmail.com>
2025-07-20 18:46:22 +0530

    Delete logger.py

--- logger.py
+++ logger.py
@@ -1,8 +1,1 @@
-import sqlite3
-db='rbac.db'
-def activity_logs(user_id,action,access_granted):
-    conn=sqlite3.connect(db)
-    cursor=conn.cursor()
-    cursor.execute('''INSERT INTO activitylogs(user_id,action,access_granted) VALUES(?,?,?)''',(user_id,action,access_granted))
-    conn.commit()
-    conn.close()
+
